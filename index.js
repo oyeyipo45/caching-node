91db1884dbaeacd936ead5b52d99db3332b3e7ac
damilola <oyeyipo45@gmail.com>
2021-10-21 10:22:53 +0100

    add cache request for 2 minutes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 
@@ -7,9 +8,17 @@ const PORT = process.env.PORT || 4000
 
 const app = express()
 
+const limiter = rateLimit({
+    windowMs: 100 * 60 * 1000,
+    max: 5
+})
+
+app.use(limiter)
+app.set('trust proxy', 1)
 
 app.use('/api', require('./routes'))
 
 app.use(cors())
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,25 @@ const router = express.Router()
 const needle = require('needle')
 const url = require('url')
 const axios = require('axios').default;
+const apicache = require('apicache');
+
 
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-router.get('/', async (req, res) => {
+let cache = apicache.middleware
+
+router.get('/',cache('2 minutes'), async (req, res) => {
     try {
 
 
     //     console.log(API_KEY_VALUE);
 
-        console.log(url.parse(req.url, true).query.q);
-        const params = new URLSearchParams({
-            [API_KEY_NAME] : API_KEY_VALUE
-        })
-
-    //     console.log(params)
+        // console.log(url.parse(req.url, true).query.q);
+        // const params = new URLSearchParams({
+        //     [API_KEY_NAME] : API_KEY_VALUE
+        // })
        
     //    const apiRes = await needle('get', `${API_BASE_URL}?${params}`);
     //     const data = apiRes.body;
@@ -27,17 +29,18 @@ router.get('/', async (req, res) => {
          const options = {
            method: 'GET',
            url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-           params: { ...url.parse(req.url, true).query.q },
+           params: { ...url.parse(req.url, true).query },
            headers: {
-             'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+             'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
              'x-rapidapi-key': process.env.API_KEY_VALUE,
            },
          };
         
-         const result = await axios.request(options);
-
-         res.status(200).json(result);
-   } catch (error) {
+        const result = await axios.request(options);
+        
+         res.status(200).json(result.data);
+    } catch (error) {
+        console.log(error)
        res.status(500).json({error});
    }
 });
